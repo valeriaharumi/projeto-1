d04a63573eea48a703266f915564c1810ac60df0
Harumi <haruumi.nishimura@gmail.com>
2020-03-31 00:24:02 -0300

    ajustes para responsividade, corpo ainda sem js

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function toggle(){
     var inicio = document.getElementById('inicio');
     inicio.classList.toggle('active')
 }
+
+
+$(document).ready(function(){
+
+    $('nav a[href*="#"]').on('click', function(){
+        $('html, body').animate(keyframes:{
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, options:2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800
+    })
+});
